cbebb28c4cbe7a0764092e8ba518e88d73214f29
Hudaiberdiev <hudaiber@ncbimac2180.be-md.ncbi.nlm.nih.gov>
2024-12-09 16:18:53 -0500

    PyTorch phase one implemented in multi-GPU

--- train_phase_one_plain.py
+++ train_phase_one_plain.py
@@ -11,12 +11,11 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torcheval.metrics import BinaryBinnedAUROC
+from torchsummary import summary
+from torch.amp import autocast, GradScaler
 import pandas as pd
 from tqdm.auto import tqdm
 import gc
-
-# from lib.trainer import train_model
-from torchsummary import summary
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -24,10 +23,12 @@ torch.multiprocessing.set_sharing_strategy('file_system')
 
 ## Hyper-parameters
 NUM_EPOCHS = 100
-BATCH_SIZE = 640
+BATCH_SIZE = 1000
+EARLY_STOP_THRESH = 15
 
 
 torch.manual_seed(1024)
+
 
 
 class PhaseOne(nn.Module):
@@ -36,33 +37,35 @@ class PhaseOne(nn.Module):
 
         self.net = nn.Sequential(
             nn.Conv1d(in_channels=4, out_channels=320, kernel_size=8, padding="same"),
+            nn.BatchNorm1d(320),
             nn.ReLU(),
             nn.Conv1d(in_channels=320, out_channels=320, kernel_size=8, padding="same"),
+            nn.BatchNorm1d(320),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.MaxPool1d(4),
 
             nn.Conv1d(in_channels=320, out_channels=480, kernel_size=8, padding="same"),
+            nn.BatchNorm1d(480),
             nn.ReLU(),
             nn.Conv1d(in_channels=480, out_channels=480, kernel_size=8, padding="same"),
+            nn.BatchNorm1d(480),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.MaxPool1d(4),
 
             nn.Conv1d(in_channels=480, out_channels=640, kernel_size=8, padding="same"),
+            nn.BatchNorm1d(640),
             nn.ReLU(),
             nn.Conv1d(in_channels=640, out_channels=640, kernel_size=8, padding="same"),
+            nn.BatchNorm1d(640),
             nn.ReLU(),
-            nn.Dropout(0.2),
-
-            # in the original TREDNet there's no MaxPooling here. This is to reduce the number of params.
-            # nn.MaxPool1d(3),
-            
+            nn.MaxPool1d(4),
             nn.Flatten(),
-            nn.LazyLinear(label_size),
+
+            nn.Linear(19840, label_size),
             nn.ReLU(),
-            nn.Linear(label_size, label_size),
-            nn.Sigmoid())
+            nn.Linear(label_size, label_size))
 
     def forward(self, x):
 
@@ -103,7 +106,7 @@ def save_checkpoint(model, optimizer, epoch, filename, val_loss, val_acc):
 
 
 def load_checkpoint(model, optimizer, filename):
-
+    
     checkpoint = torch.load(filename)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
@@ -113,17 +116,17 @@ def load_checkpoint(model, optimizer, filename):
     return epoch
 
 
-def load_dataset_wrappers(data_file):
+def load_dataset_wrappers(data_file, batch_size=BATCH_SIZE):
 
     train_dataset = HDF5Dataset(data_file, "train_data", f"train_labels")
     val_dataset   = HDF5Dataset(data_file, "validation_data", f"validation_labels")
     test_dataset  = HDF5Dataset(data_file, "test_data", f"test_labels")
 
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True,  num_workers=4, prefetch_factor=2)
-    val_loader   = DataLoader(val_dataset,   batch_size=BATCH_SIZE, shuffle=False, num_workers=2, prefetch_factor=2)
-    test_loader  = DataLoader(test_dataset,  batch_size=BATCH_SIZE, shuffle=False,  num_workers=2, prefetch_factor=2)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=4, prefetch_factor=2, pin_memory=True)
+    val_loader   = DataLoader(val_dataset,   batch_size=batch_size, shuffle=False, num_workers=2, prefetch_factor=2, pin_memory=True)
+    # test_loader  = DataLoader(test_dataset,  batch_size=batch_size, shuffle=False, num_workers=2, prefetch_factor=2, pin_memory=True)
 
-    return train_loader, val_loader, test_loader
+    return train_loader, val_loader, None
 
 
 def get_auroc(model, data_loader, device, bins=100, on_cpu=False, max_iter=-1):
@@ -199,28 +202,39 @@ def run_validation(model, data_loader, device, criterion, max_iter=-1):
 
 def train_model(model, train_loader, val_loader, device,  model_dir, max_epoch=100, max_iter=-1):
 
-    criterion = nn.BCELoss()
-    optimizer = torch.optim.Adadelta(model.parameters())
+    # criterion = nn.BCELoss()
+    criterion = nn.BCEWithLogitsLoss()
+    # optimizer = torch.optim.Adadelta(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-6, weight_decay=1e-4)
 
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', 
+                                                         factor=0.1, patience=5, 
+                                                         verbose=True)
+    
+    scaler = GradScaler('cuda')
+    
     checkpoint_file = os.path.join(model_dir, "checkpoint.base.pt")
     save_checkpoint(model, optimizer, 0, checkpoint_file, 0, 0)
 
+    best_model_file = os.path.join(model_dir, "best_model.pt")
+    best_epoch = -1
+    best_loss = 10**8
+    
     history_file = os.path.join(model_dir, "history.tsv")
     if os.path.exists(history_file):
         history = pd.read_csv(history_file, sep="\t")
     else:
         history = pd.DataFrame(columns=["epoch", "train_loss", "train_acc", "val_loss", "val_acc", "val_auroc"])
+    history.to_csv(history_file, sep="\t", index=False)
 
     model.to(device)
 
-    history.to_csv(history_file, sep="\t", index=False)
-
     for epoch in range(1, max_epoch+1):
-
+        
         model.train()
         acc_list = []
         train_loss = 0
-
+        
         with tqdm(train_loader, unit="batch", position=0, leave=True) as bar:
             bar.set_description(f"Epoch {epoch}")
 
@@ -230,10 +244,15 @@ def train_model(model, train_loader, val_loader, device,  model_dir, max_epoch=1
                 
                 optimizer.zero_grad()
 
-                y_pred = model(x)
-                loss = criterion(y_pred, y)
-                loss.backward()
-                optimizer.step()
+                with autocast('cuda'):
+                    y_pred = model(x)
+                    loss = criterion(y_pred, y)
+
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                
+                scaler.update()
+
                 train_loss += loss.item()
 
                 batch_acc = (y_pred.round() == y).float().mean()
@@ -248,21 +267,29 @@ def train_model(model, train_loader, val_loader, device,  model_dir, max_epoch=1
 
         with torch.no_grad():
             val_acc, val_loss = run_validation(model, val_loader, device, criterion, max_iter)
-            aurocs = get_auroc(model, val_loader, device, max_iter=max_iter)
-            val_auroc = aurocs.mean().numpy()
+            # aurocs = get_auroc(model, val_loader, device, max_iter=max_iter)
+            # val_auroc = aurocs.mean().numpy()
+            val_auroc = -1
 
+        scheduler.step(val_loss)
         print(f"tr acc: {train_acc:2.2f} "
               f"val acc:{val_acc:2.2f} "
               f"tr loss: {train_loss:.8f} "
               f"val loss: {val_loss:.8f}. "
               f"val auroc: {val_auroc:.3f}.")
-
-        checkpoint_file = os.path.join(model_dir, f"checkpoint.{epoch}.pt")
-        save_checkpoint(model, optimizer, epoch, checkpoint_file, val_loss, val_acc)
-
+        
         history.loc[epoch] = [epoch, train_loss, train_acc, val_loss, val_acc, val_auroc]
         history.to_csv(history_file, sep="\t", index=False)
 
+        if epoch == 0 or val_loss < best_loss:
+            best_loss = val_loss
+            best_epoch = epoch
+            save_checkpoint(model, optimizer, epoch, best_model_file, val_loss, val_acc)
+
+        elif epoch - best_epoch > EARLY_STOP_THRESH:
+            print("Early stopped training at epoch %d" % epoch)
+            break
+        
         gc.collect()
 
         print("\n\n")
@@ -271,8 +298,13 @@ def train_model(model, train_loader, val_loader, device,  model_dir, max_epoch=1
 def main(args):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    gpus = torch.cuda.device_count()
 
     model = PhaseOne()
+
+    if gpus > 1:
+        model = nn.DataParallel(model)
+
     model.to(device)
     # summary(model, (4, 1000))
 
@@ -281,13 +313,20 @@ def main(args):
 
     DATA_FILE = args.f
     SAVE_DIR = args.d
+    
 
     if not os.path.exists(SAVE_DIR):
         os.mkdir(SAVE_DIR)
 
-    train_loader, val_loader, test_loader = load_dataset_wrappers(DATA_FILE)
+    train_loader, val_loader, test_loader = load_dataset_wrappers(DATA_FILE, batch_size=BATCH_SIZE*gpus)
 
+    # for x, y in train_loader:
+    #     y_pred = model(x.to(device))
+    #     print(x.shape, y.shape, y_pred.shape)
+    # return
+    
     train_model(model, train_loader, val_loader, device, SAVE_DIR, max_iter=-1)
+
 
 
 if __name__ == "__main__":
