0fcd3023a210df6c5ca9a4ae7a055eb9f9d4eaa7
Hudaiberdiev <hudaiber@ncbimac2180.be-md.ncbi.nlm.nih.gov>
2024-12-29 16:34:06 -0500

    Trust the common wisdom: AdamW should be the default optimizer. Thanks Obama\!

--- train_phase_one_plain_L.py
+++ train_phase_one_plain_L.py
@@ -10,7 +10,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from torcheval.metrics import BinaryBinnedAUROC, BinaryAUROC, BinaryAccuracy
+from torchmetrics.classification import BinaryAUROC, BinaryAveragePrecision, BinaryPrecision, BinaryRecall, BinaryF1Score
 from torchsummary import summary
 import gc
 from tqdm import tqdm
@@ -151,6 +151,10 @@ class PhaseOneLightning(L.LightningModule):
         self.model = PhaseOne(label_size=label_size, mp=mp, do=do)
         self.criterion = nn.BCEWithLogitsLoss()
 
+        self.precision = BinaryPrecision()
+        self.recall = BinaryRecall()
+        self.f1 = BinaryF1Score()
+
     def forward(self, x):
         return self.model(x)
 
@@ -158,9 +162,17 @@ class PhaseOneLightning(L.LightningModule):
         x, y = batch
         y_pred = self(x)
         loss = self.criterion(y_pred, y)
-
         self.log('train_loss', loss, prog_bar=True, on_step=False, on_epoch=True)
-        # self.log('train_loss', loss, prog_bar=True, on_step=True, on_epoch=False)
+
+        y_pred = torch.sigmoid(y_pred)
+        precision = self.precision(y_pred, y)
+        self.log('train_prec', precision, prog_bar=False, on_step=False, on_epoch=True)
+        
+        recall = self.recall(y_pred, y)
+        self.log('train_recall', recall, prog_bar=False, on_step=False, on_epoch=True)
+
+        f1 = self.f1(y_pred, y)
+        self.log('train_f1', f1, prog_bar=False, on_step=False, on_epoch=True)
 
         return loss
 
@@ -169,9 +181,20 @@ class PhaseOneLightning(L.LightningModule):
         y_pred = self(x)
         loss = self.criterion(y_pred, y)
         self.log('val_loss', loss, prog_bar=True, on_step=False, on_epoch=True)
+        
+        y_pred = torch.sigmoid(y_pred)
+        
+        precision = self.precision(y_pred, y)
+        self.log('val_prec', precision, prog_bar=False, on_step=False, on_epoch=True)
+
+        recall = self.recall(y_pred, y)
+        self.log('val_recall', recall, prog_bar=False, on_step=False, on_epoch=True)
+        
+        f1 = self.f1(y_pred, y)
+        self.log('val_f1', f1, prog_bar=False, on_step=False, on_epoch=True)
 
         return loss
-
+    
     def configure_optimizers(self):
         if self.hparams.optimizer == "Adam":
             optimizer = torch.optim.Adam(
@@ -181,6 +204,13 @@ class PhaseOneLightning(L.LightningModule):
                 # defaulf value of 1e-08 caused NaN loss values when used with lr=0.1
                 # as a result of 16-bit mixed presicion training.
                 eps=1e-06 if self.hparams.learning_rate == 0.1 else 1e-08
+            )
+        if self.hparams.optimizer == "AdamW":
+            optimizer = torch.optim.AdamW(
+                self.parameters(),
+                lr=self.hparams.learning_rate,
+                weight_decay=self.hparams.weight_decay,
+                amsgrad=True
             )
         elif self.hparams.optimizer == "SGD":
             optimizer = torch.optim.SGD(
@@ -226,76 +256,7 @@ class PhaseOneLightning(L.LightningModule):
             }
 
 
-def main(args):
-    
-    ssd_path = "/lscratch/%s" % (os.environ.get("SLURM_JOB_ID"))
-    if os.path.exists(ssd_path):
-        new_f = os.path.join(ssd_path, os.path.basename(args.f))
-        if not os.path.exists(new_f):
-            print("Copying the data file")
-            print(f"From: {args.f}")
-            print(f"To: {new_f}\n")
-
-            shutil.copyfile(args.f, new_f)
-
-        args.f = new_f
-        print(f"New args.f = {args.f}")
-
-    model = PhaseOneLightning(
-        mp=args.mp,
-        do=args.do,
-        learning_rate=args.lr,
-        weight_decay=args.l2,
-        optimizer=args.opt
-    )
-    
-    data_module = PhaseOneDataModule(args.f, batch_size=BATCH_SIZE)
-    
-    early_stopping =  EarlyStopping(
-        monitor='val_loss',
-        patience=EARLY_STOP_THRESH,
-        mode='min',
-        verbose=True
-    )
-    
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=args.d,
-        filename='best_model',
-        save_top_k=1,
-        monitor='val_loss',
-        mode='min'
-    )
-    
-    callbacks = [checkpoint_callback] if args.ne else [early_stopping, checkpoint_callback]
-
-    csv_logger = CSVLogger(args.d, name="training_logs")
-    tb_logger = TensorBoardLogger(args.d, name="training_logs")
-    
-    trainer = L.Trainer(
-        max_epochs=NUM_EPOCHS,
-        accelerator='auto',
-        strategy='ddp', # it is the default value.
-        devices='auto',
-        callbacks=callbacks,
-        logger=[tb_logger, csv_logger],
-        precision='16-mixed',
-        log_every_n_steps=500,
-        detect_anomaly=True,
-        deterministic=True
-    )
-    
-    print("\n\n")
-    print("===== Layers of built model: =====")
-    print("==================================")
-    print(model.model.net)
-    print("==================================\n")
-    print("\n\n")
-
-    # Train the model
-    trainer.fit(model, data_module)
-
-
-def get_auroc(model, data_loader, device, bins=20, on_cpu=False, max_iter=-1):
+def get_auroc(model, data_loader, device, on_cpu=False, max_iter=-1):
 
     model.eval()
 
@@ -315,8 +276,7 @@ def get_auroc(model, data_loader, device, bins=20, on_cpu=False, max_iter=-1):
                 
                 if on_cpu:
                     y = y.to("cpu")
-                    # .to("cpu")
-
+                    
                 y_pool.append(y)
                 y_pred_pool.append(y_pred)
 
@@ -333,14 +293,11 @@ def get_auroc(model, data_loader, device, bins=20, on_cpu=False, max_iter=-1):
         for i in bar:
             _y = y[i, :]
             _y_pred = y_pred[i, :]
-            if bins == -1:
-                metric = BinaryAUROC()
-            else:
-                metric = BinaryBinnedAUROC(threshold=bins)
+            
+            metric = BinaryAUROC()
+            _y = _y.to(device)
+            _auroc = metric(_y_pred, _y)
 
-            metric
-            metric.update(_y_pred, _y)
-            (_auroc, _) = metric.compute()
             aurocs.append(_auroc)
 
     aurocs = torch.FloatTensor(aurocs)
@@ -348,12 +305,12 @@ def get_auroc(model, data_loader, device, bins=20, on_cpu=False, max_iter=-1):
     return aurocs
 
 
-def eval(args):
+def eval(args, data_prefix="test"):
 
     assert os.path.exists(args.d) and os.path.exists(args.f)
 
     checkpoint_file = os.path.join(args.d, "best_model.ckpt")
-    save_file = os.path.join(args.d, "test_set.ROC.txt")
+    save_file = os.path.join(args.d, f"{data_prefix}_set.ROC.txt")
     
     if not os.path.exists(checkpoint_file):
         raise FileNotFoundError(f"Checkpoint file doesn't exist: \n {checkpoint_file}")
@@ -367,18 +324,100 @@ def eval(args):
     
     model = PhaseOneLightning.load_from_checkpoint(checkpoint_file)
     
-    test_dataset  = HDF5Dataset(args.f, "test_data", "test_labels")
-    test_loader  = DataLoader(test_dataset,  batch_size=BATCH_SIZE, num_workers=4, prefetch_factor=2)
+    dataset  = HDF5Dataset(args.f, f"{data_prefix}_data", f"{data_prefix}_labels")
+    data_loader  = DataLoader(dataset,  batch_size=BATCH_SIZE, num_workers=4, prefetch_factor=2)
     
     device = torch.device("cuda")
     model.eval()
     model.to(device)
     
-    aurocs = get_auroc(model, test_loader, device, bins=200)
+    aurocs = get_auroc(model, data_loader, device)
     print("Test set auROC:", aurocs.mean())
     
     with open(save_file, "w") as of:
         of.write(f"auROC \t{aurocs.mean()}\n")
+
+
+def copy_to_ssd(args):
+
+    ssd_path = "/lscratch/%s" % (os.environ.get("SLURM_JOB_ID"))
+    if os.path.exists(ssd_path):
+        new_f = os.path.join(ssd_path, os.path.basename(args.f))
+        if not os.path.exists(new_f):
+            print("Copying the data file")
+            print(f"From: {args.f}")
+            print(f"To: {new_f}\n")
+            shutil.copyfile(args.f, new_f)
+
+        return new_f
+    
+    return args.f
+
+
+def main(args):
+    
+    # data_file = copy_to_ssd(args)
+    data_file = args.f
+
+    data_module = PhaseOneDataModule(data_file, batch_size=args.batch)
+    
+    model = PhaseOneLightning(mp=args.mp, do=args.do, learning_rate=args.lr, weight_decay=args.l2, optimizer=args.opt)
+
+    early_stopping =  EarlyStopping(monitor='val_loss', 
+                                    patience=args.estop, 
+                                    mode='min', 
+                                    verbose=True)
+    checkpoint_callback = ModelCheckpoint(dirpath=args.d, 
+                                          filename='best_model',
+                                          save_top_k=1, 
+                                          monitor='val_loss', 
+                                          mode='min', 
+                                          verbose=True,
+                                          save_last=True)
+    
+    callbacks = [checkpoint_callback] if args.ne else [early_stopping, checkpoint_callback]
+    csv_logger = CSVLogger(args.d, name="training_logs")
+    tb_logger = TensorBoardLogger(args.d, name="training_logs")
+
+    ###################################
+    ##########  Resume block ##########
+    ###################################
+    if args.resume:
+        checkpoint_file = os.path.join(args.d, "best_model.ckpt")
+        trainer = L.Trainer(max_epochs=args.epochs, 
+                            callbacks=callbacks,
+                            logger=[tb_logger, csv_logger],
+                            detect_anomaly=False)
+        trainer.fit(model, data_module, ckpt_path=checkpoint_file)
+
+        return
+
+    ##############################################
+    ########## New model training block ##########
+    ##############################################
+
+    trainer = L.Trainer(
+        max_epochs=args.epochs,
+        accelerator='auto',
+        strategy='ddp', # it is the default value.
+        devices='auto',
+        callbacks=callbacks,
+        logger=[tb_logger, csv_logger],
+        precision='16-mixed',
+        log_every_n_steps=500,
+        # detect_anomaly=True,
+        deterministic=True
+    )
+    
+    print("\n\n")
+    print("===== Layers of built model: =====")
+    print("==================================")
+    print(model.model.net)
+    print("==================================\n")
+    print("\n\n")
+
+    # Train the model
+    trainer.fit(model, data_module)
 
 
 def parse_args():
@@ -388,14 +427,19 @@ def parse_args():
     parser.add_argument('-d', type=str, required=True,help='Directory to save model')
     parser.add_argument('-f', type=str, required=True, help='Dataset file')
     parser.add_argument('-eval', action="store_true", help='Run evaluation of the trained model')
+    parser.add_argument('-resume', action="store_true", help='Resume the training task from the checkpoint')
 
     parser.add_argument('-mp', type=int, default=0, help='Max pooling layer at the end')
     parser.add_argument('-lr', type=float, default=1e-2, help='Learning rate')
     parser.add_argument('-l2', type=float, default=1e-5, help='L2 regularizer')
     parser.add_argument('-do', type=float, default=0.2, help='Dropout')
     parser.add_argument('-ne', action="store_true", help='Exclude early stopping callback from training')
+    
+    parser.add_argument('-opt', choices=['SGD', 'Adam', 'Adadelta', 'AdamW'], default="Adam")
 
-    parser.add_argument('-opt', choices=['SGD', 'Adam', 'Adadelta'], default="Adam")
+    parser.add_argument('-epochs', type=int, default=100, help='Number of epochs to train')
+    parser.add_argument('-estop', type=int, default=10, help='Early stopping patience')
+    parser.add_argument('-batch', type=int, default=1000, help='Batch size')
     
     if len(sys.argv) < 2:
         parser.print_usage()
@@ -414,12 +458,42 @@ def parse_args():
 
 if __name__ == "__main__":
 
+    print("#########################")
+    print("### Python/CUDA envs  ###")
+    print(f"Python interpreter: {sys.executable}")
+    print(f"torch.__version__: {torch.__version__}")
+    print(f"torch.cuda.is_available(): {torch.cuda.is_available()}")
+    print(f"torch.cuda.device_count(): {torch.cuda.device_count()}")
+    print(f"torch.cuda.get_device_name(0): {torch.cuda.get_device_name(0)}")
+    print("#########################")
+    print("\n\n")
+    
     args = parse_args()
+    checkpoint_file = os.path.join(args.d, "best_model.ckpt")
 
     if args.eval:
-        eval(args)
-        sys.exit()
+        if not os.path.exists(checkpoint_file):
+            print(f"No checkpoint file was found to run evaluation: {checkpoint_file}")
+            sys.exit(1)
+
+        eval(args, data_prefix="test")
+        # eval(args, data_prefix="validation")
+        sys.exit(1)
     
+    if not os.path.exists(args.f):
+        print(f"Data file path doesn't exist: {args.f}")
+        sys.exit(1)
+    
+    if not os.path.exists(args.d):
+        print(f"Directory path doesn't exist: {args.d}")
+        sys.exit(1)
+
+    if args.resume:
+        
+        if not os.path.exists(checkpoint_file):
+            print(f"No checkpoint file was found to resume from: {checkpoint_file}")
+            sys.exit(1)
+
     main(args)
 
 
